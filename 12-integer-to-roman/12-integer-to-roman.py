d4c8bd7c8577288bfc57e061c1fc18a1ab9ef892
Shariful Islam <sharifultroublesome@gmail.com>
2022-01-06 13:37:38 +0600

    Time: 55 ms (46.29%), Space: 14.3 MB (28.71%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,36 @@
+import bisect
+import collections
+
+class Solution:
+    integer_numbers = {
+        1 : "I",
+        4 : "IV",
+        5 : "V",
+        9 : "IX",
+        10: "X",
+        40 : "XL",
+        50 : "L",
+        90 : "XC",
+        100 : "C",
+        400 : "CD",
+        500 : "D",
+        900 : "CM",
+        1000 : "M",
+    }
+    
+    def intToRoman(self, num: int) -> str:
+        result = ""
+        
+        while(num):
+            max_key = 0
+            for key in self.integer_numbers:
+                if (key <= num):
+                    max_key = key
+                else:
+                    break;
+
+            result += self.integer_numbers[max_key]
+            num -= max_key
+            
+        return result
+    
