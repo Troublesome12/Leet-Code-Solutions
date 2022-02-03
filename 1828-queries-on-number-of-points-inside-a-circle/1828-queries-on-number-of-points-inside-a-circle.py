e7cfe726a09dcf36fdfdc9dca0211134474b1e75
Shariful Islam <sharifultroublesome@gmail.com>
2022-02-03 15:43:38 +0600

    Time: 4485 ms (5.05%), Space: 14.2 MB (94.86%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.py
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.py
@@ -2,7 +2,7 @@ class Solution:
     def countPointsOnCircle(self, points: List[List[int]], circle: List[int]) -> int:
         count = 0
         for point in points:
-            if abs(point[0]-circle[0])**2 + abs(point[1]-circle[1])**2 <= abs(circle[2])**2:
+            if (point[0]-circle[0])**2 + (point[1]-circle[1])**2 <= (circle[2])**2:
                 count += 1
         
         return count
